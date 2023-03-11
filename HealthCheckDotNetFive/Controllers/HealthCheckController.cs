5183239ef8278caa615361b18f478837be194b6b
Vladimir Makarov <makarov86@gmail.com>
2023-03-11 22:13:03 +0300

    Init commit

---
+++ HealthCheckDotNetFive/Controllers/HealthCheckController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace HealthCheckDotNetFive.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IHealthStatusInfo HealthCheck()
+        {
+            return new HealthResponse { StatusName = "ОК" };
+        }
+
+        [HttpGet("uptime")]
+        public IActionResult UpTime()
+        {
+            var span = (DateTime.Now - Process.GetCurrentProcess().StartTime);
+            var info = $"Up time: {Math.Round(span.TotalHours)} hours {span.Minutes:D2} minutes {span.Seconds:D2} seconds";
+
+            return Ok(new { UpTimeInfo = info });
+        }
+
+        [HttpGet("Info")]
+        public IActionResult Info()
+        {
+            return BadRequest(new HealthResponse { StatusName = "34" });
+        }
+    }
+
+    public interface IHealthStatusInfo
+    {
+        public string StatusName { get; set; }
+    }
+
+    public class HealthResponse : IHealthStatusInfo
+    {
+        [JsonPropertyName("status")]
+        public string StatusName { get; set; }
+    }
+}
---
+++ HealthCheckDotNetFive/Controllers/ImgController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
+
+namespace HealthCheckDotNetFive.Controllers
+{
+    [Route("img")]
+    [ApiController]
+    public class ImgController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetImage()
+        {
+            var client = new HttpClient();
+
+            try
+            {
+                Thread.Sleep(70000);
+                var bytes = await client.GetStreamAsync(
+                    "https://icdn.lenta.ru/images/2023/03/05/19/20230305193707882/owl_detail_240_a2bdb05ed87262b135e472b7165d0777.jpeg");
+
+                var file = File(bytes, MediaTypeNames.Image.Jpeg, DateTimeOffset.Now.AddDays(-4), EntityTagHeaderValue.Any, true);
+                file.EnableRangeProcessing = true;
+
+                return File(bytes, MediaTypeNames.Image.Jpeg, DateTimeOffset.Now.AddDays(-4), EntityTagHeaderValue.Any,
+                    true);
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+    }
+}
